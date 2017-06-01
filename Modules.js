3f6fb8e14eef0142010ff78e5e80ddd5c7ad381a
Vladimir Liannoy <Vladimir.Liannoy@netent.com>
2017-06-01 09:14:39 +0300

    minor improvement;

--- Modules.js
+++ Modules.js
@@ -1,28 +1,3 @@
 var modules = [
         "AnimationManager", "Text", "Button", "AdvancedButton"
-],
-    createdModules = {},
-    i, componenets, module;
-
-
-
-for(i = 0; i < modules.length; i++){
-    createdModules[modules[i]] = new window[modules[i]];
-    module = createdModules[modules[i]];
-    componenets = module.getDefaultMVCClasses();
-    module.model = new componenets.model(modules[i]);
-    module.controller = new componenets.controller(modules[i]);
-    module.view = new componenets.view(modules[i]);
-
-    module.controller.model = module.model;
-    module.controller.view = module.view;
-
-    module.view.model = module.model;
-
-    module.model.setupData();
-    module.controller.setupEvents();
-    module.view.init();
-    console.error(module);
-}
-
-
+];
